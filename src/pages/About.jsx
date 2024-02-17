a4fbbf54915dd1cfbe1a35fe2730cd1aaaeeeca3
Sifat-Ikram <sifatikram@gmail.com>
2024-02-17 20:30:36 +0600

    download part updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,12 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 
 
 const About = () => {
-    const resumePdfPath = 'https://drive.google.com/file/d/1BL7VK7WJOsE12clRnYslQW3kVC3payH2/view?usp=sharing';
+    const resumePdfPath = '/Md_Sifat_Ikram_Resume.pdf'; // Path to resume PDF in public folder
 
     const handleDownloadClick = () => {
         const downloadLink = document.createElement('a');
         downloadLink.href = resumePdfPath;
-        downloadLink.download = 'Md. Sifat Ikram_Resume.pdf';
+        downloadLink.download = 'Md_Sifat_Ikram_Resume.pdf';
         document.body.appendChild(downloadLink);
         downloadLink.click();
         document.body.removeChild(downloadLink);
@@ -24,7 +24,7 @@ const About = () => {
                 <h1 className='mb-8 text-3xl font-semibold'>
                     Every great design begin with an even better story
                 </h1>
-                <h2  className='mb-5 text-2xl'>
+                <h2 className='mb-5 text-2xl'>
                     I am a passionate and motivated  web developer with a keen interest in creating dynamic and user-friendly websites. My journey into the world of web development started with a fascination for turning ideas into interactive and visually appealing digital experiences.
                 </h2>
                 <button onClick={handleDownloadClick} className='flex items-center gap-3 px-4 py-2 mt-8 font-semibold text-white bg-sky-700 rounded-xl'><FaCloudDownloadAlt className='text-lg' /> <span className="text-xl">Download Resume</span></button>
