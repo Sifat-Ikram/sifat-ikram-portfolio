8f2652158b77def1ed7f4d2585e0d13e3af63634
Sifat-Ikram <sifatikram@gmail.com>
2025-01-30 20:56:58 +0600

    portfolio updated

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -11,7 +11,7 @@ const Experience = () => {
     <div className="space-y-8">
       <button className="flex items-center shadow bg-base-300 dark:border-[2px] border-solid border-white dark:hover:bg-white dark:hover:text-dark dark:bg-dark dark:text-white gap-2 px-4 py-2 mb-5 rounded-full cursor-pointer hover:text-white hover:bg-[#041C93]">
         <FaLaptopCode />
-        <h1 className="font-medium uppercase">Education</h1>
+        <h1 className="font-medium uppercase">Experience</h1>
       </button>
       <div data-aos="flip-up">
         <div className="px-3 py-3 shadow space-y-3 rounded-lg bg-base-200 dark:bg-dark dark:border-[2px] border-solid border-white hover:border-[#233bb1]">
